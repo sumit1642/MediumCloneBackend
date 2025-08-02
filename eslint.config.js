9f6b41d1abf0c69dd18748d9e83ef2e33ed188e5
Sumit1642 <sumeetyadav1642@gmail.com>
2025-08-02 16:20:14 +0530

    formatting

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,6 @@ import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
 ]);
